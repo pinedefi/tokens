c75d5e547e645411c817bd182e1efa6d392938cd
Alexander Ho <39906305+piske-alex@users.noreply.github.com>
2023-01-29 14:30:45 -0500

    Update sbvePINE.sol

--- sbvePINE.sol
+++ sbvePINE.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.3;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
